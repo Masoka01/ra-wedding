d501244441419daf20deee3f258075e602077936
Dimas Mayoni <143168338+Masoka01@users.noreply.github.com>
2025-03-13 14:56:56 +0700

    produk

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const showMoreBtn = document.getElementById("showMoreBtn");
-  const produkItems = document.querySelectorAll(".produk-item.d-none");
+  // Fungsi untuk mengatur Show More / Show Less
+  function setupShowMore(buttonId, sectionId) {
+    const showMoreBtn = document.getElementById(buttonId);
+    const produkItems = document.querySelectorAll(
+      `#${sectionId} .produk-item.d-none`
+    );
 
-  let isExpanded = false;
+    let isExpanded = false;
 
-  showMoreBtn.addEventListener("click", function () {
-    produkItems.forEach((item) => {
-      item.classList.toggle("d-none");
+    showMoreBtn.addEventListener("click", function () {
+      produkItems.forEach((item) => {
+        item.classList.toggle("d-none");
+      });
+
+      if (isExpanded) {
+        showMoreBtn.textContent = "Show More";
+      } else {
+        showMoreBtn.textContent = "Show Less";
+      }
+
+      isExpanded = !isExpanded;
     });
+  }
 
-    if (isExpanded) {
-      showMoreBtn.textContent = "Show More";
-    } else {
-      showMoreBtn.textContent = "Show Less";
-    }
+  // Atur Show More / Show Less untuk bagian Kasih
+  setupShowMore("showMoreBtnKasih", "produk-kasih");
 
-    isExpanded = !isExpanded;
-  });
+  // Atur Show More / Show Less untuk bagian Sayang
+  setupShowMore("showMoreBtnSayang", "produk-sayang");
+
+  // Atur Show More / Show Less untuk bagian Sayang
+  setupShowMore("showMoreBtnKangen", "produk-kangen");
+
+  // Atur Show More / Show Less untuk bagian Sayang
+  setupShowMore("showMoreBtnSpesial", "produk-spesial");
 });
